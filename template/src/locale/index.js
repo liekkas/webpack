045c33f82ca9c7d3c52abcc0c7f673b32ad7a963
liekkas <zjq0717@163.com>
2018-05-15 16:39:03 +0800

    add description

--- template/src/locale/index.js
+++ template/src/locale/index.js
@@ -1,6 +1,7 @@
 export default {
   'zh-CN': {
-    projectName: '前端模板演示',
+    projectName: {{ description }},
+    description: \{{ description }},
     copyright: '© 2018 All Rights Reserved 广州市捍御者信息科技有限公司',
   }
 };
